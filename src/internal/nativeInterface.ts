e0f38e3b613c534e0c547f384a00495478c891f1
Mirko Pira <30497555+mirkodevx@users.noreply.github.com>
2021-04-10 10:41:08 +0200

    Update nativeInterface.ts
    Hotfix: android build release

--- src/internal/nativeInterface.ts
+++ src/internal/nativeInterface.ts
@@ -5,7 +5,7 @@ let RNDeviceInfo: DeviceInfoNativeModule | undefined = NativeModules.RNDeviceInf
 
 // @ts-ignore
 if (Platform.OS === 'web' || Platform.OS === 'dom') {
-  RNDeviceInfo = require('../web');
+  RNDeviceInfo = require(process.cwd()+'/'../web');
 }
 
 if (!RNDeviceInfo) {
